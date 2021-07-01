3edfdd380b2e08aae3c3742de31d507df29b7fc3
NethajiChowdary <nethajichowdary555@gmail.com>
2021-07-01 16:05:32 +0530

    Added: Code to Create a Simple Linked List

---
+++ src/linkedlist/Node.java
@@ -0,0 +1,17 @@
+package linkedlist;
+
+public class Node {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to LinkedList Program");
+		
+		
+		LinkedList linkedList = new LinkedList();
+		linkedList.insert(56);
+		linkedList.insert(30);
+		linkedList.insert(70);
+		
+		linkedList.print();
+	}
+
+}
